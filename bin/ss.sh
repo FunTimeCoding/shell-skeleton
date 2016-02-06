df45fb1a95cc465d323336f29ee130fb0e15c57d
Alexander Reitzel <funtimecoding@gmail.com>
2016-02-06 14:51:15 +0100

    add sample code

--- bin/ss.sh
+++ bin/ss.sh
@@ -1,1 +1,13 @@
 #!/bin/sh -e
+
+DIRECTORY=$(dirname "${0}")
+SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
+
+usage()
+{
+    echo "Usage: ${0}"
+}
+
+# shellcheck source=/dev/null
+. "${SCRIPT_DIRECTORY}/../lib/shell_skeleton.sh"
+echo "Hello friend."
--- lib/shell_skeleton.sh
+++ lib/shell_skeleton.sh
@@ -1,1 +1,45 @@
 #!/bin/sh -e
+
+CONFIG=""
+VERBOSE=false
+
+function_exists()
+{
+    declare -f -F "${1}" > /dev/null
+
+    return $?
+}
+
+while true; do
+    case ${1} in
+        --config)
+            CONFIG=${2-}
+            shift 2
+            ;;
+        --help)
+            echo "Global usage: ${0} [--verbose][--debug][--help][--config CONFIG]"
+
+            if function_exists usage; then
+                usage
+            fi
+
+            exit 0
+            ;;
+        --verbose)
+            VERBOSE=true
+            echo "Verbose mode enabled."
+            shift
+            ;;
+        --debug)
+            set -x
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+OPTIND=1
+export CONFIG
+export VERBOSE
