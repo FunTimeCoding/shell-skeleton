90a8e7e61e4239cfa02636444ef3ec7571e6e9fc
Alexander Reitzel <funtimecoding@gmail.com>
2018-03-07 18:34:38 +0100

    synchronize the synchronize and initialize scripts

--- script/skeleton/initialize.sh
+++ script/skeleton/initialize.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -e
 
-CAMEL=$(echo "${1}" | grep -E '^([A-Z]+[a-z0-9]*){1,}$') || CAMEL=""
+NAME=$(echo "${1}" | grep -E '^([A-Z]+[a-z0-9]*){1,}$') || NAME=""
 
-if [ "${CAMEL}" = "" ]; then
-    echo "Usage: ${0} UpperCamelCaseName"
+if [ "${NAME}" = "" ]; then
+    echo "Usage: ${0} NAME"
+    echo "Name must be in upper camel case."
 
     exit 1
 fi
@@ -19,10 +20,10 @@ else
 fi
 
 rm -rf script/skeleton
-DASH=$(echo "${CAMEL}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
-INITIALS=$(echo "${CAMEL}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
+DASH=$(echo "${NAME}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
+INITIALS=$(echo "${NAME}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
 UNDERSCORE=$(echo "${DASH}" | ${SED} -E 's/-/_/g')
 # shellcheck disable=SC2016
-${FIND} . -type f -regextype posix-extended ! -regex '^.*/(\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/ShellSkeleton/${2}/g" -e "s/shell-skeleton/${3}/g" -e "s/shell_skeleton/${4}/g" -e "s/bin\/ss/bin\/${5}/g" "${6}"' '_' "${SED}" "${CAMEL}" "${DASH}" "${UNDERSCORE}" "${INITIALS}" '{}' \;
+${FIND} . -type f -regextype posix-extended ! -regex '^.*/(\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/ShellSkeleton/${2}/g" -e "s/shell-skeleton/${3}/g" -e "s/shell_skeleton/${4}/g" -e "s/bin\/ss/bin\/${5}/g" "${6}"' '_' "${SED}" "${NAME}" "${DASH}" "${UNDERSCORE}" "${INITIALS}" '{}' \;
 git mv lib/shell_skeleton.sh lib/"${UNDERSCORE}".sh
 git mv bin/ss bin/"${INITIALS}"
--- script/skeleton/synchronize.sh
+++ script/skeleton/synchronize.sh
@@ -9,15 +9,15 @@ if [ "${TARGET}" = "" ]; then
 fi
 
 if [ ! -d "${TARGET}" ]; then
-    echo "Target directory ${TARGET} does not exist."
+    echo "Target directory does not exist."
 
     exit 1
 fi
 
-CAMEL=$(head -n 1 "${TARGET}"/README.md | awk '{ print $2 }' | grep --extended-regexp '^([A-Z]+[a-z0-9]*){1,}$') || CAMEL=""
+NAME=$(head -n 1 "${TARGET}"/README.md | awk '{ print $2 }' | grep --extended-regexp '^([A-Z]+[a-z0-9]*){1,}$') || NAME=""
 
-if [ "${CAMEL}" = "" ]; then
-    echo "Could not determine project name in ${TARGET}."
+if [ "${NAME}" = "" ]; then
+    echo "Could not determine the project name."
 
     exit 1
 fi
@@ -25,11 +25,11 @@ fi
 SYSTEM=$(uname)
 
 if [ "${SYSTEM}" = Darwin ]; then
-    FIND="gfind"
-    SED="gsed"
+    FIND='gfind'
+    SED='gsed'
 else
-    FIND="find"
-    SED="sed"
+    FIND='find'
+    SED='sed'
 fi
 
 cp ./*.md "${TARGET}"
@@ -38,10 +38,10 @@ cp -R documentation/* "${TARGET}/documentation"
 mkdir -p "${TARGET}/script"
 cp -R script/* "${TARGET}/script"
 cp .gitignore "${TARGET}"
-DASH=$(echo "${CAMEL}" | ${SED} --regexp-extended 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
-INITIALS=$(echo "${CAMEL}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
-UNDERSCORE=$(echo "${DASH}" | ${SED} --regexp-extended 's/-/_/g')
 cd "${TARGET}" || exit 1
 rm -rf script/skeleton
+DASH=$(echo "${NAME}" | ${SED} --regexp-extended 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
+INITIALS=$(echo "${NAME}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
+UNDERSCORE=$(echo "${DASH}" | ${SED} --regexp-extended 's/-/_/g')
 # shellcheck disable=SC2016
-${FIND} . -type f -regextype posix-extended ! -regex '^.*/(build|\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/ShellSkeleton/${2}/g" -e "s/shell-skeleton/${3}/g" -e "s/shell_skeleton/${4}/g" -e "s/bin\/ss/bin\/${5}/g" "${6}"' '_' "${SED}" "${CAMEL}" "${DASH}" "${UNDERSCORE}" "${INITIALS}" '{}' \;
+${FIND} . -type f -regextype posix-extended ! -regex '^.*/(build|\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/ShellSkeleton/${2}/g" -e "s/shell-skeleton/${3}/g" -e "s/shell_skeleton/${4}/g" -e "s/bin\/ss/bin\/${5}/g" "${6}"' '_' "${SED}" "${NAME}" "${DASH}" "${UNDERSCORE}" "${INITIALS}" '{}' \;
