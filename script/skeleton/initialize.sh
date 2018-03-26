873e78d7b4568a46e2c23dcb58d9fc12c262142e
Alexander Reitzel <funtimecoding@gmail.com>
2018-03-26 18:05:42 +0200

    use --regexp-extended for sed

--- script/skeleton/initialize.sh
+++ script/skeleton/initialize.sh
@@ -20,9 +20,9 @@ else
 fi
 
 rm -rf script/skeleton
-DASH=$(echo "${NAME}" | ${SED} --extended-regexp 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
+DASH=$(echo "${NAME}" | ${SED} --regexp-extended 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
 INITIALS=$(echo "${NAME}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
-UNDERSCORE=$(echo "${DASH}" | ${SED} --extended-regexp 's/-/_/g')
+UNDERSCORE=$(echo "${DASH}" | ${SED} --regexp-extended 's/-/_/g')
 # shellcheck disable=SC2016
 ${FIND} . -type f -regextype posix-extended ! -regex '^.*/(\.git|\.idea)/.*$' -exec sh -c '${1} -i --expression "s/ShellSkeleton/${2}/g" --expression "s/shell-skeleton/${3}/g" --expression "s/shell_skeleton/${4}/g" --expression "s/bin\/ss/bin\/${5}/g" "${6}"' '_' "${SED}" "${NAME}" "${DASH}" "${UNDERSCORE}" "${INITIALS}" '{}' \;
 git mv lib/shell_skeleton.sh "lib/${UNDERSCORE}.sh"
