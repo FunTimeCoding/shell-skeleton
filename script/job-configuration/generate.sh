0cd6bc66eeb985fe9918913519dd6d3cb1a5acc8
Alexander Reitzel <funtimecoding@gmail.com>
2018-05-26 18:03:24 +0200

    Undo converting github locators to https

--- script/job-configuration/generate.sh
+++ script/job-configuration/generate.sh
@@ -8,7 +8,9 @@ if [ "${IS_GITHUB}" = true ]; then
 
     if [ "${IS_SSH}" = true ]; then
         # TODO: GitHub does not allow to give read only permission to personal repositories. Only to organization repositories. In order to not have to create an organization, use HTTPS locators for jobs. Also to not have to create one SSH key per repository, because GitHub only allows to use one public key once.
-        REMOTE="https://github.com/${REMOTE#*:}"
+        # TODO: It does not matter. A machine user has SSH read access to public repositories anyway.
+        #REMOTE="https://github.com/${REMOTE#*:}"
+        true
     fi
 fi
 
