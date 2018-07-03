c51176baba964bcb7288e62839c90239741f4f9a
FunTimeCoding <funtimecoding@gmail.com>
2018-07-03 12:01:12 +0200

    ignore vendor when syncing with php-skeleton

--- lib/common.sh
+++ lib/common.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
 
-EXCLUDE_FILTER='^.*/(build|tmp|\.git|\.vagrant|\.idea)/.*$'
+# vendor is in here to not break php-skeleton based projects when synchronizing with them.
+EXCLUDE_FILTER='^.*/(build|tmp|vendor|\.git|\.vagrant|\.idea)/.*$'
 export EXCLUDE_FILTER
