381e5019c8d9e72ff64b172843a7f263e3254b5a
Alexander Reitzel <funtimecoding@gmail.com>
2018-07-26 17:14:46 +0200

    add development option to docker run script

--- script/docker/run.sh
+++ script/docker/run.sh
@@ -1,13 +1,21 @@
 #!/bin/sh -e
 
+# Development mode mounts the project root so it can be edited and re-ran without rebuilding the image and recreating the container.
+
+if [ "${1}" = --development ]; then
+    DEVELOPMENT=true
+else
+    DEVELOPMENT=false
+fi
+
 docker ps --all | grep --quiet shell-skeleton && FOUND=true || FOUND=false
 
 if [ "${FOUND}" = false ]; then
-    # TODO: This is the production setup that copies the code into the container image. How to decide whether to mount it or copy the code into the container in the Dockerfile?
-    #docker run --name shell-skeleton funtimecoding/shell-skeleton
-
-    # TODO: This is the development setup that mounts the project root so it can be changed and re-ran without rebuilding. Should this stay as a run.sh argument?
-    docker run --name shell-skeleton --volume $(pwd):/shell-skeleton funtimecoding/shell-skeleton
+    if [ "${DEVELOPMENT}" = true ]; then
+        docker run --name shell-skeleton --volume $(pwd):/shell-skeleton funtimecoding/shell-skeleton
+    else
+        docker run --name shell-skeleton funtimecoding/shell-skeleton
+    fi
 
     # TODO: Specifying the entry point overrides CMD in Dockerfile. Is this useful, or should all sub commands go through one entry point script? I'm inclined to say one entry point script per project.
     #docker run --name shell-skeleton --volume $(pwd):/shell-skeleton --entrypoint /shell-skeleton/bin/other.sh funtimecoding/shell-skeleton
