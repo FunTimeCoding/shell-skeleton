d0429f5811824a15b1c42e7f4dfb50ee3381bff0
Alexander Reitzel <funtimecoding@gmail.com>
2018-08-28 17:26:14 +0200

    replace initials in Dockerfile too

--- script/skeleton/synchronize.sh
+++ script/skeleton/synchronize.sh
@@ -58,4 +58,4 @@ INITIALS=$(echo "${NAME}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:
 UNDERSCORE=$(echo "${DASH}" | ${SED} --regexp-extended 's/-/_/g')
 # shellcheck disable=SC2016
 ${FIND} . -regextype posix-extended -type f ! -regex "${EXCLUDE_FILTER}" -exec sh -c '${1} --in-place --expression "s/ShellSkeleton/${2}/g" --expression "s/shell-skeleton/${3}/g" --expression "s/shell_skeleton/${4}/g" "${5}"' '_' "${SED}" "${NAME}" "${DASH}" "${UNDERSCORE}" '{}' \;
-${SED} --in-place --expression "s/bin\/ss/bin\/${INITIALS}/g" --expression "s/'ss'/'${INITIALS}'/g" README.md Vagrantfile
+${SED} --in-place --expression "s/bin\/ss/bin\/${INITIALS}/g" --expression "s/'ss'/'${INITIALS}'/g" README.md Vagrantfile Dockerfile
