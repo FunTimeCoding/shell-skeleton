3e24952c781b4f18dc208c67bd3da120b74e8c6a
Alexander Reitzel <funtimecoding@gmail.com>
2018-08-28 20:46:33 +0200

    filter venv in python skeleton

--- lib/common.sh
+++ lib/common.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 
 # vendor is in here to not break php-skeleton based projects when synchronizing with them.
-EXCLUDE_FILTER='^.*/(build|tmp|vendor|\.git|\.vagrant|\.idea)/.*$'
+# .venv is for python-skeleton.
+EXCLUDE_FILTER='^.*/(build|tmp|vendor|\.venv|\.git|\.vagrant|\.idea)/.*$'
 export EXCLUDE_FILTER
