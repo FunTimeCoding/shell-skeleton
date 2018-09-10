815f76d4536b274510e92bbaa9d11e91f80633f7
Alexander Reitzel <funtimecoding@gmail.com>
2018-09-10 11:44:35 +0200

    filter target directory of maven when replacing

--- lib/common.sh
+++ lib/common.sh
@@ -3,5 +3,6 @@
 # vendor is in here to not break php-skeleton based projects when synchronizing with them.
 # .venv is for python-skeleton.
 # node_modules is for java-script-skeleton
-EXCLUDE_FILTER='^.*/(build|tmp|vendor|node_modules|\.venv|\.git|\.vagrant|\.idea)/.*$'
+# target is for java-skeleton
+EXCLUDE_FILTER='^.*/(build|tmp|vendor|node_modules|target|\.venv|\.git|\.vagrant|\.idea)/.*$'
 export EXCLUDE_FILTER
