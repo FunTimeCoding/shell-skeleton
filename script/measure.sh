6b1f17aac007ef62d54ed487670882a014506ee2
Alexander Reitzel <funtimecoding@gmail.com>
2019-01-15 19:09:20 +0100

    ignore tmp and build directories

--- script/measure.sh
+++ script/measure.sh
@@ -42,7 +42,7 @@ if [ "${1}" = --ci-mode ]; then
     if [ -f "${HOME}/.sonar-qube-tools.sh" ]; then
         # shellcheck source=/dev/null
         . "${HOME}/.sonar-qube-tools.sh"
-        sonar-scanner "-Dsonar.projectKey=${PROJECT_NAME}" -Dsonar.sources=. "-Dsonar.host.url=${SONAR_SERVER}" "-Dsonar.login=${SONAR_LOGIN}" | "${TEE}" build/log/sonar-runner.log
+        sonar-scanner "-Dsonar.projectKey=${PROJECT_NAME}" -Dsonar.sources=. "-Dsonar.host.url=${SONAR_SERVER}" "-Dsonar.login=${SONAR_LOGIN}" '-Dsonar.exclusions=build/**,tmp/**' | "${TEE}" build/log/sonar-runner.log
     else
         echo "SonarQube configuration missing."
 
