1e9aab67ed19c06bf60e8e4a0471d54701b45906
Alexander Reitzel <funtimecoding@gmail.com>
2019-07-28 19:59:03 +0200

    venv is not required

--- script/vagrant/create.bat
+++ script/vagrant/create.bat
@@ -1,6 +1,4 @@
 @echo off
-if not exist .venv python -m venv .venv
-
 if not exist tmp mkdir tmp
 
 if not exist tmp\bootstrap-salt.sh powershell -Command "Invoke-WebRequest https://bootstrap.saltstack.com -OutFile tmp/bootstrap-salt.sh"
