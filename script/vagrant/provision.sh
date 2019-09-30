85fa1a68819bb2ca2ee262e8eeba384532cf417c
Alexander Reitzel <funtimecoding@gmail.com>
2019-10-01 00:37:42 +0200

    install ansible

--- script/vagrant/provision.sh
+++ script/vagrant/provision.sh
@@ -8,7 +8,10 @@ if [ "${CODENAME}" = jessie ]; then
     dpkg-reconfigure --frontend noninteractive tzdata
     apt-get --quiet 2 install vim multitail htop tree git
 elif [ "${CODENAME}" = stretch ]; then
+    cp /vagrant/configuration/backports.txt /etc/apt/sources.list.d/backports.list
+    apt-get --quiet 2 update
     apt-get --quiet 2 install neovim multitail htop tree git shellcheck hunspell devscripts ruby-ronn
+    apt-get --quiet 2 install ansible --target-release stretch-backports
 elif [ "${CODENAME}" = buster ]; then
-    apt-get --quiet 2 install neovim multitail htop tree git shellcheck hunspell devscripts ruby-ronn
+    apt-get --quiet 2 install neovim multitail htop tree git shellcheck hunspell devscripts ruby-ronn ansible
 fi
