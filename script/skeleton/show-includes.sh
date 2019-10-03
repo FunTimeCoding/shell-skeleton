ba8a896aca39e276e13c665cc5954bfd6e235bd7
Alexander Reitzel <funtimecoding@gmail.com>
2019-10-03 14:31:48 +0200

    add scripts to show includes and excludes

---
+++ script/skeleton/show-includes.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -e
+
+DIRECTORY=$(dirname "${0}")
+SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
+# shellcheck source=/dev/null
+. "${SCRIPT_DIRECTORY}/../../configuration/project.sh"
+TARGET="${1}"
+
+if [ "${TARGET}" = '' ]; then
+    echo "Usage: ${0} TARGET"
+
+    exit 1
+fi
+
+if [ ! -d "${TARGET}" ]; then
+    echo "Target directory does not exist."
+
+    exit 1
+fi
+
+NAME=$(head -n 1 "${TARGET}/README.md" | awk '{ print $2 }' | grep --extended-regexp '^([A-Z]+[a-z0-9]*){1,}$') || NAME=''
+
+if [ "${NAME}" = '' ]; then
+    echo "Could not determine the project name."
+
+    exit 1
+fi
+
+SYSTEM=$(uname)
+
+if [ "${SYSTEM}" = Darwin ]; then
+    FIND='gfind'
+    SED='gsed'
+else
+    FIND='find'
+    SED='sed'
+fi
+
+cd "${TARGET}" || exit 1
+${FIND} . -regextype posix-extended -type f -regex "${INCLUDE_FILTER}"
