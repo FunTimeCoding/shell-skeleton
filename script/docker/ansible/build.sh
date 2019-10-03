0818aff2cefe712d3918cb65e7ec8804bcb50752
Alexander Reitzel <funtimecoding@gmail.com>
2019-10-03 17:03:58 +0200

    add Dockerfile installing ansible

---
+++ script/docker/ansible/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+DIRECTORY=$(dirname "${0}")
+SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
+# shellcheck source=/dev/null
+. "${SCRIPT_DIRECTORY}/../../configuration/project.sh"
+
+docker build --tag "${VENDOR_NAME_LOWER}/ansible-playbook" script/docker/ansible
