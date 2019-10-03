6f4d91ffa5e4779acf99cde85d229a414c636a4a
Alexander Reitzel <funtimecoding@gmail.com>
2019-10-03 19:11:33 +0200

    use lower key build arg

--- script/docker/ansible/build.sh
+++ script/docker/ansible/build.sh
@@ -7,4 +7,4 @@ SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 
 docker build --tag "${VENDOR_NAME_LOWER}/ansible-playbook" script/docker/ansible/playbook
 PUBLIC_KEY=$(cat "${HOME}/.ssh/id_rsa.pub")
-docker build --tag "${VENDOR_NAME_LOWER}/ansible-ssh" --build-arg SSH_PUBLIC_KEY="${PUBLIC_KEY}" script/docker/ansible/ssh
+docker build --tag "${VENDOR_NAME_LOWER}/ansible-ssh" --build-arg ssh_public_key="${PUBLIC_KEY}" script/docker/ansible/ssh
