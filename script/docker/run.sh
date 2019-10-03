2da1142347c3f1abe7947e3c7520f1d1c6e10793
Alexander Reitzel <funtimecoding@gmail.com>
2019-10-03 16:18:24 +0200

    shift the development argument away, pass all arguments to docker

--- script/docker/run.sh
+++ script/docker/run.sh
@@ -7,12 +7,15 @@ SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 
 if [ "${1}" = --development ]; then
     DEVELOPMENT=true
+    shift
 else
     DEVELOPMENT=false
 fi
 
 if [ "${DEVELOPMENT}" = true ]; then
-    docker run --interactive --tty --rm --name "${PROJECT_NAME_DASH}" --volume "${WORKING_DIRECTORY}:/${PROJECT_NAME_DASH}" "${VENDOR_NAME_LOWER}/${PROJECT_NAME_DASH}"
+    # shellcheck disable=SC2068
+    docker run --interactive --tty --rm --name "${PROJECT_NAME_DASH}" --volume "${WORKING_DIRECTORY}:/${PROJECT_NAME_DASH}" "${VENDOR_NAME_LOWER}/${PROJECT_NAME_DASH}" $@
 else
-    docker run --interactive --tty --rm --name "${PROJECT_NAME_DASH}" "${VENDOR_NAME_LOWER}/${PROJECT_NAME_DASH}"
+    # shellcheck disable=SC2068
+    docker run --interactive --tty --rm --name "${PROJECT_NAME_DASH}" "${VENDOR_NAME_LOWER}/${PROJECT_NAME_DASH}" $@
 fi
