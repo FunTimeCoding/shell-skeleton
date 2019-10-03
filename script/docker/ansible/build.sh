b1ac01549a6e315df126c54523f9195d5dd96406
Alexander Reitzel <funtimecoding@gmail.com>
2019-10-03 19:18:40 +0200

    file must be where dockerfile is

--- script/docker/ansible/build.sh
+++ script/docker/ansible/build.sh
@@ -6,6 +6,6 @@ SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 . "${SCRIPT_DIRECTORY}/../../../configuration/project.sh"
 
 docker build --tag "${VENDOR_NAME_LOWER}/ansible-playbook" script/docker/ansible/playbook
-mkdir -p tmp
-cp "${HOME}/.ssh/id_rsa.pub" tmp/id_rsa.pub
+mkdir -p script/docker/ansible/ssh/tmp
+cp "${HOME}/.ssh/id_rsa.pub" script/docker/ansible/ssh/tmp/id_rsa.pub
 docker build --tag "${VENDOR_NAME_LOWER}/ansible-ssh" script/docker/ansible/ssh
