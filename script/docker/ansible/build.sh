0dd1beda13fc81d307e413d6a37b532101f054f2
Alexander Reitzel <funtimecoding@gmail.com>
2019-10-03 17:08:16 +0200

    fix source path

--- script/docker/ansible/build.sh
+++ script/docker/ansible/build.sh
@@ -3,6 +3,6 @@
 DIRECTORY=$(dirname "${0}")
 SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 # shellcheck source=/dev/null
-. "${SCRIPT_DIRECTORY}/../../configuration/project.sh"
+. "${SCRIPT_DIRECTORY}/../../../configuration/project.sh"
 
 docker build --tag "${VENDOR_NAME_LOWER}/ansible-playbook" script/docker/ansible
