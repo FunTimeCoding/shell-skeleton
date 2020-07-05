542fb0569c79053e621d252f7b528e28a3f73c51
Alexander Reitzel <funtimecoding@gmail.com>
2020-07-06 00:57:26 +0200

    add cent-os support

--- script/vagrant/provision.sh
+++ script/vagrant/provision.sh
@@ -1,17 +1,24 @@
 #!/bin/sh -e
 
-export DEBIAN_FRONTEND=noninteractive
-CODENAME=$(lsb_release --codename --short)
+grep --quiet CentOS /etc/os-release && CENTOS=true || CENTOS=false
 
-if [ "${CODENAME}" = jessie ]; then
-    echo Europe/Berlin >/etc/timezone
-    dpkg-reconfigure --frontend noninteractive tzdata
-    apt-get --quiet 2 install vim multitail htop tree git dos2unix
-elif [ "${CODENAME}" = stretch ]; then
-    cp /vagrant/configuration/backports.txt /etc/apt/sources.list.d/backports.list
-    apt-get --quiet 2 update
-    apt-get --quiet 2 install neovim multitail htop tree git shellcheck hunspell devscripts ruby-ronn dos2unix
-    apt-get --quiet 2 install ansible --target-release stretch-backports
-elif [ "${CODENAME}" = buster ]; then
-    apt-get --quiet 2 install neovim multitail htop tree git shellcheck hunspell devscripts ronn dos2unix ansible
+if [ "${CENTOS}" = true ]; then
+    yum install --assumeyes vim
+    # TODO: Install NeoVim https://github.com/neovim/neovim/wiki/Installing-Neovim
+else
+    export DEBIAN_FRONTEND=noninteractive
+    CODENAME=$(lsb_release --codename --short)
+
+    if [ "${CODENAME}" = jessie ]; then
+        echo Europe/Berlin >/etc/timezone
+        dpkg-reconfigure --frontend noninteractive tzdata
+        apt-get --quiet 2 install vim multitail htop tree git dos2unix
+    elif [ "${CODENAME}" = stretch ]; then
+        cp /vagrant/configuration/backports.txt /etc/apt/sources.list.d/backports.list
+        apt-get --quiet 2 update
+        apt-get --quiet 2 install neovim multitail htop tree git shellcheck hunspell devscripts ruby-ronn dos2unix
+        apt-get --quiet 2 install ansible --target-release stretch-backports
+    elif [ "${CODENAME}" = buster ]; then
+        apt-get --quiet 2 install neovim multitail htop tree git shellcheck hunspell devscripts ronn dos2unix ansible
+    fi
 fi
