425b1152e6abe7aa8fef7b4cadf7b434ef89a4dc
Alexander Reitzel <funtimecoding@gmail.com>
2020-10-07 20:45:13 +0200

    block gs initials

--- configuration/project.sh
+++ configuration/project.sh
@@ -23,7 +23,8 @@ export PROJECT_NAME_UNDERSCORE
 PROJECT_NAME_INITIALS=$(echo "${PROJECT_NAME_CAMEL}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
 BLOCKED_INITIALS='ps
 pu
-fg'
+fg
+gs'
 
 echo "${PROJECT_NAME_INITIALS}" | grep --quiet "^${BLOCKED_INITIALS}$" && ARE_INITIALS_BLOCKED=true || ARE_INITIALS_BLOCKED=false
 
